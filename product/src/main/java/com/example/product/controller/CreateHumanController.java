aa4eadcd501733c410499e8488c2629cfc32ad85
wesley <17724238651@163.com>
2023-12-04 17:28:44 +0800

    增加es模块

--- product/src/main/java/com/example/product/controller/CreateHumanController.java
+++ product/src/main/java/com/example/product/controller/CreateHumanController.java
@@ -1,10 +1,18 @@
 package com.example.product.controller;
 
 import com.example.product.service.create_human.*;
+import com.vladsch.flexmark.parser.Parser;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.util.ast.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author wsj
@@ -20,5 +28,17 @@ public class CreateHumanController {
         humanFactory.createHuman(YellowHuman.class).speak();
         humanFactory.createHuman(WhiteHuman.class).speak();
         humanFactory.createHuman(BlackHuman.class).speak();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String  text = "# Hello, *world*!  \n\nThis is a **Markdown** document.";
+        String filePath = "/Users/wesley/Downloads/test.md";
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        //Parser parser = Parser.builder().build();
+        //Document document = parser.parse(text);
+        //HtmlRenderer renderer = HtmlRenderer.builder().build();
+        //String html = renderer.render(document);
+        Path path = Paths.get(filePath);
+        Files.write(path, text.getBytes());
     }
 }
